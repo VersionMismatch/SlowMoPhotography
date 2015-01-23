0064b262c4c04470b194f478725e341e93318198
Leandro Lopez <lean.trek@gmail.com>
2015-01-22 21:39:30 -0300

    SlowMoPhotography starting commit.

---
+++ SlowMoPhotography/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SlowMoPhotography
+//
+//  Created by Leandro Lopez on 1/22/15.
+//  Copyright (c) 2015 VersionMismatch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
