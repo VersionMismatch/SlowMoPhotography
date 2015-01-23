5e7cc8293f49e33075c1090fddbadc54f1578b27
Leandro Lopez <lean.trek@gmail.com>
2015-01-22 22:13:12 -0300

    Adding basic views and prototype layout.

---
+++ SlowMoPhotography/ViewControllers/SMPCaptureViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMPCaptureViewController.h
+//  SlowMoPhotography
+//
+//  Created by Leandro Lopez on 1/22/15.
+//  Copyright (c) 2015 VersionMismatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMPCaptureViewController : UIViewController
+
+@end
